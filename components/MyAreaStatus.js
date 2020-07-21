3d99bf5305467794c489942ad50a7ed3585093b1
unknown <ahnsunnyyoung@gmail.com>
2020-07-21 13:02:08 +0900

    add area title

--- components/MyAreaStatus.js
+++ components/MyAreaStatus.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+function circle(){
+  return(
+     <View>
+       
+     </View> 
+  );
+}
+
 export default function MyAreaStatus(){
 
   return(
     <View style={styles.main}>
-        <Text>
-            여기에는 내 지역 정보를 카드뷰로!
+        <Text style={styles.areaName}>
+            서울
         </Text>
+        
     </View>
   );
 }
 
 const styles = StyleSheet.create({
     main: {
-        height: '300%'
-    }
+    },
+    areaName: {
+        fontSize: 40,
+        width: '100%',
+        textAlign: 'center',
+        marginTop: 30,
+        marginBottom: 30,
+        fontWeight: '600'
+    },
 })
