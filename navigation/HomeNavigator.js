fcb77feb5fc0105bd9cb7551e173d7c573a815b9
Hyejin00 <svivs137@naver.com>
2020-07-21 13:35:55 +0900

    Add menu icon to header, Modify header sstyle

--- navigation/HomeNavigator.js
+++ navigation/HomeNavigator.js
@@ -1,16 +1,37 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { TouchableWithoutFeedback } from 'react-native';
 
 const HomeStack = createStackNavigator();
 
-export default function HomeNavigator() {
+export default function HomeNavigator({ navigation, route }) {
   return (
     <HomeStack.Navigator>
       <HomeStack.Screen
         name="HomeScreen"
         component={HomeScreen}
-        options={{ headerTitle: 'Home' }}
+        options={{ 
+          headerTitle: '',
+          headerLeft: () => 
+          { 
+            return (
+            <TouchableWithoutFeedback
+            onPress={()=>{navigation.openDrawer()}}>
+              <Ionicons
+              name= 'md-menu'
+              size={24}
+              color='black'
+              style={{marginLeft:20}}/>
+          </TouchableWithoutFeedback>
+          )},
+          headerStyle:{
+            backgroundColor: null,
+            elevation: 0,
+            shadowOpacity: 0,
+          }
+      }}
       />
     </HomeStack.Navigator>
   );
