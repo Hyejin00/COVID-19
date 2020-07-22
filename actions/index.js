41e5bec01a1b5e0c22f2ca34f6ca07b2c0f262dd
Minji Kim <wisdomin121@naver.com>
2020-07-22 18:33:17 +0900

    Fetch News data

--- actions/index.js
+++ actions/index.js
@@ -11,12 +11,20 @@ const NEWS_HEADER = {
 export function fetchCOVIDNews(){
   return async (dispatch) => {
     try{
-      const result = await axios(NEWS_API, {params: {
-        query: "코로나",
-        display: 50,
-        start: 1,
-        sort: "sim"
-      }}, NEWS_HEADER);
+      const result = await axios(NEWS_API, 
+        {
+          params: {
+            query: "코로나",
+            display: 50,
+            start: 1,
+            sort: "sim"
+          },
+          headers: {
+            'X-Naver-Client-Id': NEWS_ID,
+            'X-Naver-Client-Secret': NEWS_PW
+          }
+        }
+        );
       dispatch({type: 'FETCH_COVID_NEWS', payload: result.data.items})
     }catch(error){
       console.error(error);
