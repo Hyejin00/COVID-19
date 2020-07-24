e9b9387ce748b69a1e71d04cc6702468ca3a8b2a
Minji Kim <wisdomin121@naver.com>
2020-07-24 16:25:04 +0900

    Add Marker in Map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useSelector } from 'react-redux';
 import Constants from 'expo-constants';
@@ -8,12 +8,10 @@ import Constants from 'expo-constants';
 import MapHeader from '../components/map/MapHeader';
 import colors from '../constants/Colors'
 
-<script
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB_aX-q10zRK9ryYyWMIjVKaol8F63Erek"
-/>
 
 export default function MapScreen(){
   const myAreaData = useSelector(state => state.myAreaData);
+  const point = {x:35.4, y:127.8};
 
   return(
     <LinearGradient
@@ -24,13 +22,144 @@ export default function MapScreen(){
       <View style={styles.mapview}>
         <MapView 
           style={styles.mapStyle}
+          scrollEnabled={false}
           initialRegion={{
             latitude: 35.4,
             longitude: 127.8,
             latitudeDelta: 5,
             longitudeDelta: 5
           }} 
-        />
+        >
+          <Marker
+            title='서울'
+            description={"안녕 난 서울이야~"}
+            centerOffset={point}
+            coordinate={{
+              latitude: 37.5642135,
+              longitude: 127.0016985
+            }}
+          />
+          <Marker
+            title='경기'
+            centerOffset={point}
+            coordinate={{
+              latitude: 37.897100,
+              longitude: 127.150334
+            }}
+          />
+           <Marker
+            title='강원'
+            coordinate={{
+              latitude: 37.810724,
+              longitude: 128.306424
+            }}
+          />
+          <Marker
+            title='인천'
+            coordinate={{
+              latitude: 37.455983,
+              longitude: 126.692074
+            }}
+          />
+          <Marker
+            title='충북'
+            coordinate={{
+              latitude: 36.997731,
+              longitude: 127.698314
+            }}
+          />
+          <Marker
+            title='대전'
+            coordinate={{
+              latitude: 36.338966,
+              longitude: 127.394956
+            }}
+          />
+          <Marker
+            title='세종'
+            coordinate={{
+              latitude: 36.561683,
+              longitude: 127.256361
+            }}
+          />
+          <Marker
+            title='충남'
+            coordinate={{
+              latitude: 36.717639,
+              longitude: 126.797042
+            }}
+          />
+          <Marker
+            title='전북'
+            coordinate={{
+              latitude: 35.713506,
+              longitude: 127.150068
+            }}
+          />
+          <Marker
+            title='광주'
+            coordinate={{
+              latitude: 35.160435,
+              longitude: 126.832109
+            }}
+          />
+          <Marker
+            title='전남'
+            coordinate={{
+              latitude: 34.870603,
+              longitude: 126.990984
+            }}
+          />
+          <Marker
+            title='제주'
+            coordinate={{
+              latitude: 33.380933,
+              longitude: 126.546734
+            }}
+          />
+          <Marker
+            title='경남'
+            coordinate={{
+              latitude: 35.455266,
+              longitude: 128.215065
+            }}
+          />
+          <Marker
+            title='부산'
+            coordinate={{
+              latitude: 35.161740,
+              longitude: 129.043971
+            }}
+          />
+          <Marker
+            title='울산'
+            coordinate={{
+              latitude: 35.547750,
+              longitude: 129.250494
+            }}
+          />
+          <Marker
+            title='대구'
+            coordinate={{
+              latitude: 35.831567,
+              longitude: 128.559324
+            }}
+          />
+          <Marker
+            title='경북'
+            coordinate={{
+              latitude: 36.296001,
+              longitude: 128.890377
+            }}
+          />
+           <Marker
+            title='검역'
+            coordinate={{
+              latitude: 34.418866,
+              longitude: 129.154003
+            }}
+          />
+        </MapView>
       </View>
 		</LinearGradient>
   );
@@ -50,5 +179,8 @@ const styles = StyleSheet.create({
   mapStyle: {
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height,
+  },
+  markerStyle: {
+    
   }
 });
